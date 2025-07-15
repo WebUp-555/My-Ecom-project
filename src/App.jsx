d86f108b82819f5e0bca652727e1d72dbee3cff1
WebUp-555 <designcore.dev@gmail.com>
2025-07-15 22:26:44 +0530

    all working including links

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
-import Products from './Pages/Products'
-import Home from './Pages/Home';
 
+
+import Products from './Pages/Products';
+import Home from './Pages/Home';
 import SignIn from './Pages/signin';
 import SignUp from './Pages/signup';
-import ProductDetails from "./Pages/ProductsDetails";
-import CartPage from "./Pages/cartPage";
-
-
-
+import ProductDetails from './Pages/ProductsDetails';
+import CartPage from './Pages/cartPage';
 
 function App() {
   return (
@@ -20,7 +18,7 @@ function App() {
   );
 }
 
-// This separates routing logic from BrowserRouter
+// Routing logic
 function AppContent() {
   const location = useLocation();
   const hideNavbarOnRoutes = ['/signin', '/signup'];
@@ -31,12 +29,11 @@ function AppContent() {
       {shouldShowNavbar && <Navbar />}
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Products" element={<Products />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/Products" element={<Products />}/>
+        <Route path="/Products" element={<Products />} />
         <Route path="/product/:id" element={<ProductDetails />} />
-        <Route path="/cart" element={<CartPage/>} />
+        <Route path="/cart" element={<CartPage />} />
         
       </Routes>
     </>
